d7a2c9baaf86043295c5937459fed1fe7353fdb2
Jose Enmanuel Estrella <enmanuelgit@gmail.com>
2020-07-16 09:30:46 -0400

    Add docs

---
+++ docs/precache-manifest.d21096206c837b0f32504904f1190928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af209543bb93a83f4ab5e6cd4b63ba97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63da2a04b0a5020f7fcb",
+    "url": "/static/css/main.bd707560.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63da2a04b0a5020f7fcb",
+    "url": "/static/js/main.d587531d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
